8f08c6d85b1bb58a33d5b27b637218df71bb6bc4
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-09-06 06:11:57 +0100

    Add files via upload

---
+++ sentence2funct.js
@@ -0,0 +1,23 @@
+// sillySentence('excited', 'love', 'functions'); 
+// Should return 'I am so excited because I love coding! Time to write some more awesome functions!'
+
+//string interpolation
+
+const sillySentence = (adjective, verb, noun) => `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`
+
+/*
+// With string concatenation:
+const sillySentence = (adjective, verb, noun) => 'I am so ' + adjective + ' because I ' + verb +  ' coding! Time to write some more awesome ' + noun + '!'
+
+
+// As a function declaration:
+function sillySentence(adjective, verb, noun) {
+    return `I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`
+}
+*/
+
+
+// Uncomment the line below when you're ready to try out your function
+// console.log(sillySentence('excited', 'love', 'functions')) 
+
+// We encourage you to add more function calls of your own to test your code!
